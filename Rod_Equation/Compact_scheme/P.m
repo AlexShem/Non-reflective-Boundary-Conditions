580eccfb72dde84a55f37a80bf512d208c309a9d
Aleksandr Shemendyuk <alex.shemendyuk@gmail.com>
2020-04-05 17:09:31 +0200

    Update P.m

--- Rod_Equation/Compact_scheme/P.m
+++ Rod_Equation/Compact_scheme/P.m
@@ -1,16 +1,4 @@
 function Legandre = P(n, x)
-%     global Pcoef;
-%     if isempty(Pcoef)
-%         load Pcoef.mat
-%     end
-%     if length(Pcoef) < n + 1 || isempty(Pcoef{n + 1})
-%         for k = length(Pcoef) + 1 : n + 1
-%             n1 = k - 2;
-%             Pcoef{k} = [Pcoef{k - 1} 0]*(2*n1 + 1)/(n1 + 1) -  [0 0 Pcoef{k - 2}]*n1/(n1 + 1);
-%         end
-%         save Pcoef.mat Pcoef
-%     end
-%     Legandre = polyval(Pcoef{n + 1}, x);
 
     if n == 0
         Legandre = 1;
@@ -30,7 +18,7 @@ function Legandre = P(n, x)
     for j = 2 : n
         main_cf = main_cf * (2*j - 1) / j / 2;
     end
-%     main_cf = main_cf / 2^(n - 1);
+    
     ind = 2;
     for j = n - 2 : -2 : c
         for k = 1 : (n - j) / 2
