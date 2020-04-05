a6d66f77c649e5ae351be26d1000195e3730096f
Aleksandr Shemendyuk <alex.shemendyuk@gmail.com>
2020-04-05 19:35:40 +0200

    Change to corresponding name

--- Rod_Equation/Compact_scheme/Code/eta_series_num.m
+++ Rod_Equation/Compact_scheme/Code/eta_series_num.m
@@ -1,14 +1,14 @@
-function [eta_ser_minus] = eta_series_num(nu, mu, N_eta)
-    a = 3/(12*mu + 4) - .5; b = 9*nu/(3*mu + 1) - 2; c = 1 - (12*nu + 3) / (6*mu + 2); d = 3*nu / (6*mu + 2);
+function [eta_ser_plus] = eta_series_num(nu, mu, N_eta)
+%     a = 3/(12*mu + 4) - .5; b = 9*nu/(3*mu + 1) - 2; c = 1 - (12*nu + 3) / (6*mu + 2); d = 3*nu / (6*mu + 2);
 
     x = 1+72*nu/(6*mu-1)^2;
     leg_sum = zeros(1, N_eta + 2);
     for n = 0 : N_eta + 1
         leg_sum(end - n) = P(n, x);
     end
-    eta_ser_minus = conv([1, -2*x, 1], leg_sum);
-    eta_ser_minus = conv(eta_ser_minus, [-1, 1]);
-    eta_ser_minus = polysum([1, -2*(-12*nu+6*mu-1)/(6*mu-1), 1], -eta_ser_minus);
-    eta_ser_minus = (6*mu-1)/(12*nu) * eta_ser_minus;
-    eta_ser_minus = eta_ser_minus(1 : end-1);
+    eta_ser_plus = conv([1, -2*x, 1], leg_sum);
+    eta_ser_plus = conv(eta_ser_plus, [-1, 1]);
+    eta_ser_plus = polysum([1, -2*(-12*nu+6*mu-1)/(6*mu-1), 1], -eta_ser_plus);
+    eta_ser_plus = (6*mu-1)/(12*nu) * eta_ser_plus;
+    eta_ser_plus = eta_ser_plus(1 : end-1);
 end
