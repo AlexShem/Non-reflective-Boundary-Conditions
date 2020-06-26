2aad3c7060624f4ba342a19290cb9d9579c2f07d
AlexShem <alex.shemendyuk@gmail.com>
2020-06-26 17:30:18 +0200

    Clean: delete forgotten code

--- Rod_Equation/Compact_scheme/Programs_for_check/mysqrt.m
+++
@@ -1,8 +0,0 @@
-function [y_1, y_2] = mysqrt(x)
-    r = abs(x);
-    phi = angle(x);
-    ind = phi < 0;
-    phi(ind) = phi(ind) + 2*pi;
-    y_1 = sqrt(r) .* exp(1i * phi/2);
-    y_2 = -y_1;
-end
