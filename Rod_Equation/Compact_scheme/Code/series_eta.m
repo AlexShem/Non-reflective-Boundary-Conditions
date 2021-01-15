2c4fb713be884d7a49273a5ac46a98cd2a75c1e1
AlexShem <alex.shemendyuk@gmail.com>
2021-01-15 16:25:40 +0100

    Update: uncheked

--- Rod_Equation/Compact_scheme/Code/series_eta.m
+++ Rod_Equation/Compact_scheme/Code/series_eta.m
@@ -2,6 +2,11 @@ function [eta_ser_minus, eta_ser_plus] = series_eta(nu, mu, N_terms)
 % eta_1 == eta minus (const + const * w + const * w^2 + ...)
 % eta_2 == eta plus  (const + const * w + const * w^2 + ...) / w
 
+a = 3/(12*mu + 4) - .5;
+b = (9*nu)/(3*mu + 1) - 2;
+c = 1 - (12*nu + 3)/(6*mu + 2);
+d = (3*nu)/(6*mu + 2);
+
 x = 1+72*nu/(6*mu-1)^2;
 
 legandre_sum = zeros(1, N_terms + 2);
@@ -19,4 +24,7 @@ eta_ser_plus = (6*mu-1)/(12*nu) * eta_ser_plus;
 % Return polynomial of degree N_terms
 % Note that the constant term inside of big brackets is zero
 eta_ser_minus = eta_ser_minus(end - N_terms - 1 : end-1);
+
+% Remember that \eta_2(end) == const / w
+eta_ser_plus = eta_ser_plus(end - N_terms - 1 : end);
 end
